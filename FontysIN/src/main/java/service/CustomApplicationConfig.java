0f20073959891146634d987ce7e31b428f26b4e5
Alayoubi,Ranim R <r.alayoubi@student.fontys.nl>
2020-09-29 14:33:26 +0200

    The project Setup is ready!

---
+++ FontysIN/src/main/java/service/CustomApplicationConfig.java
@@ -0,0 +1,18 @@
+package service;
+
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CustomApplicationConfig extends ResourceConfig
+{
+    public CustomApplicationConfig()
+    {
+        packages("service.resources");
+
+        register(new LoggingFeature(Logger.getLogger(LoggingFeature.DEFAULT_LOGGER_NAME),
+                Level.INFO, LoggingFeature.Verbosity.PAYLOAD_ANY, LoggingFeature.DEFAULT_MAX_ENTITY_SIZE));
+    }
+}
