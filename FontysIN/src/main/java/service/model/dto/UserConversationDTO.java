613d6d5143a8ba528af07b2965d95620abdf6db6
Rawan Abou Dehn <r.aboudehn@student.fontys.nl>
2020-11-16 14:50:45 +0100

    Create main messaging classes

---
+++ FontysIN/src/main/java/service/model/dto/UserConversationDTO.java
@@ -0,0 +1,87 @@
+package service.model.dto;
+
+import service.model.Conversation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserConversationDTO {
+    private int id;
+    private int profileId;
+    private String firstName;
+    private String lastName;
+    private String image;
+    private List<Conversation> conversations;
+
+    public UserConversationDTO(int id, int profileId, String firstName, String lastName, String image) {
+        this.id = id;
+        this.profileId = profileId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.image = image;
+        this.conversations = new ArrayList<>();
+    }
+
+    public UserConversationDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getProfileId() {
+        return profileId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProfileId(int profileId) {
+        this.profileId = profileId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public List<Conversation> getConversations() {
+        return conversations;
+    }
+
+    public void setConversations(List<Conversation> conversations) {
+        this.conversations = conversations;
+    }
+
+    @Override
+    public String toString() {
+        return "UserConversationDTO{" +
+                "id=" + id +
+                ", profileId=" + profileId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", image='" + image + '\'' +
+                ", conversations=" + conversations +
+                '}';
+    }
+}
