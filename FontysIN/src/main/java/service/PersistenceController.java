2fe0a098dfb1f1201680fa051e20066e4ccbdded
i439426 <i439426@fhict.nl>
2020-12-05 11:14:01 +0100

    post/comment/like refactoring

--- FontysIN/src/main/java/service/PersistenceController.java
+++ FontysIN/src/main/java/service/PersistenceController.java
@@ -18,258 +18,258 @@ import java.util.StringTokenizer;
 public class PersistenceController {
 
     // Post section
-
-    public List<Posts> getPosts(){
-        JDBCPosts postsRepository = new JDBCPosts();
-
-        try {
-            List<Posts> posts = (List<Posts>) postsRepository.getPosts();
-
-            System.out.println("ok");
-
-            return posts;
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public List<Posts> getPostsByDate(){
-        JDBCPosts postsRepository = new JDBCPosts();
-
-        try {
-            List<Posts> posts = (List<Posts>) postsRepository.getPostsByDate();
-
-            System.out.println("ok");
-
-            return posts;
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public List<Posts> getNewsfeed(int uId){
-        JDBCPosts postsRepository = new JDBCPosts();
-
-        try {
-            List<Posts> posts = (List<Posts>) postsRepository.getNewsfeed(uId);
-
-            for (Posts post: posts) {
-                System.out.println(post.getId());
-            }
-
-            return posts;
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-    public List<Posts> getPostByUserId(int uId){
-        JDBCPosts postsRepository = new JDBCPosts();
-
-        try {
-            List<Posts> posts = (List<Posts>) postsRepository.getPostsByUserId(uId);
-
-            System.out.println("ok");
-
-            return posts;
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-    public Posts getPost(int Id){
-        JDBCPosts postsRepository = new JDBCPosts();
-
-        try {
-            Posts post = (Posts) postsRepository.getPost(Id);
-
-            System.out.println("ok");
-
-            return post;
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public boolean addPost(Posts post){
-        JDBCPosts postsRepository = new JDBCPosts();
-
-        try {
-            return postsRepository.addPosts(post);
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public boolean updatePost(Posts post){
-        JDBCPosts postsRepository = new JDBCPosts();
-
-        try {
-            return postsRepository.updatePost(post);
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public boolean deletePost(Posts post){
-        JDBCPosts postsRepository = new JDBCPosts();
-
-        try {
-            return postsRepository.deletePost(post);
-        } catch (DatabaseException | SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    // End of Post section
-    // Comment section
-
-    public List<Comments> getCommets(){
-        JDBCComments commentsRepository = new JDBCComments();
-
-        try {
-            List<Comments> comments = (List<Comments>) commentsRepository.getComments();
-
-
-
-            return comments;
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public List<Comments> getCommetsByPostId(int pId){
-        JDBCComments commentsRepository = new JDBCComments();
-
-        try {
-            List<Comments> comments = (List<Comments>) commentsRepository.getCommentsByPostId(pId);
-
-
-
-            return comments;
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public Comments getCommet(int Id){
-        JDBCComments commentsRepository = new JDBCComments();
-
-        try {
-            Comments comm = (Comments) commentsRepository.getComment(Id);
-
-
-
-            return comm;
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public boolean addComment(Comments comm){
-        JDBCComments commentsRepository = new JDBCComments();
-
-        try {
-            return commentsRepository.addComm(comm);
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public boolean updateComment(Comments comm){
-        JDBCComments commentsRepository = new JDBCComments();
-
-        try {
-            return commentsRepository.updateComm(comm);
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public boolean deleteComment(Comments comm){
-        JDBCComments commentsRepository = new JDBCComments();
-
-        try {
-            return commentsRepository.deleteComment(comm);
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    //End of Comment section
-    //Like section
-    public List<Like> getLikes(){
-        JDBCLikeRepository likeRepository = new JDBCLikeRepository();
-
-        try {
-            List<Like> likes = (List<Like>) likeRepository.getLikes();
-
-            return likes;
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public List<Like> getLikesByPost(int id){
-        JDBCLikeRepository likeRepository = new JDBCLikeRepository();
-
-        try {
-            List<Like> likes = (List<Like>) likeRepository.getLikesByPost(id);
-
-            return likes;
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public Like getPostLikesByUser(int id,int userId){
-        JDBCLikeRepository likeRepository = new JDBCLikeRepository();
-
-        try {
-            Like like = (Like) likeRepository.getPostLikeByUSer(id,userId);
-
-            return like;
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public boolean addLike (Like like){
-        JDBCLikeRepository likeRepository = new JDBCLikeRepository();
-
-        try {
-            return likeRepository.addLike(like);
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public boolean deleteLike(Like like){
-        JDBCLikeRepository likeRepository = new JDBCLikeRepository();
-
-        try {
-            return likeRepository.deleteLike(like);
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
+//
+//    public List<Posts> getPosts(){
+//        JDBCPosts postsRepository = new JDBCPosts();
+//
+//        try {
+//            List<Posts> posts = (List<Posts>) postsRepository.getPosts();
+//
+//            System.out.println("ok");
+//
+//            return posts;
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//    public List<Posts> getPostsByDate(){
+//        JDBCPosts postsRepository = new JDBCPosts();
+//
+//        try {
+//            List<Posts> posts = (List<Posts>) postsRepository.getPostsByDate();
+//
+//            System.out.println("ok");
+//
+//            return posts;
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//    public List<Posts> getNewsfeed(int uId){
+//        JDBCPosts postsRepository = new JDBCPosts();
+//
+//        try {
+//            List<Posts> posts = (List<Posts>) postsRepository.getNewsfeed(uId);
+//
+//            for (Posts post: posts) {
+//                System.out.println(post.getId());
+//            }
+//
+//            return posts;
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//    public List<Posts> getPostByUserId(int uId){
+//        JDBCPosts postsRepository = new JDBCPosts();
+//
+//        try {
+//            List<Posts> posts = (List<Posts>) postsRepository.getPostsByUserId(uId);
+//
+//            System.out.println("ok");
+//
+//            return posts;
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//    public Posts getPost(int Id){
+//        JDBCPosts postsRepository = new JDBCPosts();
+//
+//        try {
+//            Posts post = (Posts) postsRepository.getPost(Id);
+//
+//            System.out.println("ok");
+//
+//            return post;
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//    public boolean addPost(Posts post){
+//        JDBCPosts postsRepository = new JDBCPosts();
+//
+//        try {
+//            return postsRepository.addPosts(post);
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+//
+//    public boolean updatePost(Posts post){
+//        JDBCPosts postsRepository = new JDBCPosts();
+//
+//        try {
+//            return postsRepository.updatePost(post);
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+//
+//    public boolean deletePost(Posts post){
+//        JDBCPosts postsRepository = new JDBCPosts();
+//
+//        try {
+//            return postsRepository.deletePost(post);
+//        } catch (DatabaseException | SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+//
+//    // End of Post section
+//    // Comment section
+//
+//    public List<Comments> getCommets(){
+//        JDBCComments commentsRepository = new JDBCComments();
+//
+//        try {
+//            List<Comments> comments = (List<Comments>) commentsRepository.getComments();
+//
+//
+//
+//            return comments;
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//    public List<Comments> getCommetsByPostId(int pId){
+//        JDBCComments commentsRepository = new JDBCComments();
+//
+//        try {
+//            List<Comments> comments = (List<Comments>) commentsRepository.getCommentsByPostId(pId);
+//
+//
+//
+//            return comments;
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//    public Comments getCommet(int Id){
+//        JDBCComments commentsRepository = new JDBCComments();
+//
+//        try {
+//            Comments comm = (Comments) commentsRepository.getComment(Id);
+//
+//
+//
+//            return comm;
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//    public boolean addComment(Comments comm){
+//        JDBCComments commentsRepository = new JDBCComments();
+//
+//        try {
+//            return commentsRepository.addComm(comm);
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+//
+//    public boolean updateComment(Comments comm){
+//        JDBCComments commentsRepository = new JDBCComments();
+//
+//        try {
+//            return commentsRepository.updateComm(comm);
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+//
+//    public boolean deleteComment(Comments comm){
+//        JDBCComments commentsRepository = new JDBCComments();
+//
+//        try {
+//            return commentsRepository.deleteComment(comm);
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+//
+//    //End of Comment section
+//    //Like section
+//    public List<Like> getLikes(){
+//        JDBCLikeRepository likeRepository = new JDBCLikeRepository();
+//
+//        try {
+//            List<Like> likes = (List<Like>) likeRepository.getLikes();
+//
+//            return likes;
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//    public List<Like> getLikesByPost(int id){
+//        JDBCLikeRepository likeRepository = new JDBCLikeRepository();
+//
+//        try {
+//            List<Like> likes = (List<Like>) likeRepository.getLikesByPost(id);
+//
+//            return likes;
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//    public Like getPostLikesByUser(int id,int userId){
+//        JDBCLikeRepository likeRepository = new JDBCLikeRepository();
+//
+//        try {
+//            Like like = (Like) likeRepository.getPostLikeByUSer(id,userId);
+//
+//            return like;
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//    public boolean addLike (Like like){
+//        JDBCLikeRepository likeRepository = new JDBCLikeRepository();
+//
+//        try {
+//            return likeRepository.addLike(like);
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+//
+//    public boolean deleteLike(Like like){
+//        JDBCLikeRepository likeRepository = new JDBCLikeRepository();
+//
+//        try {
+//            return likeRepository.deleteLike(like);
+//        } catch (DatabaseException e) {
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
 
 
 
