259c4f66d5fe581f6f76fd40916304464e181f41
Alayoubi,Ranim R <r.alayoubi@student.fontys.nl>
2020-12-10 15:39:28 +0100

    Refactor the back end
    JDBCUserRepository is added

--- FontysIN/src/main/java/service/controller/UserController.java
+++ FontysIN/src/main/java/service/controller/UserController.java
@@ -2,6 +2,7 @@ package service.controller;
 
 import service.PersistenceController;
 import service.model.*;
+import service.model.dto.UserDTO;
 import service.repository.DatabaseException;
 import service.repository.JDBCProfileRepository;
 
@@ -114,5 +115,244 @@ public class UserController {
         }
 
         return "";
+    }
+
+    /******************RANIM***********************Filter Search One bu One**************************/
+
+    /**
+     * Show/print the users with the given type
+     * @param type of the user to be shown.
+     */
+    //show users by user type
+    public List<UserDTO> UserFilteredWithType(UserType type){
+
+        JDBCUserRepository userRepository = new JDBCUserRepository();
+
+        try {
+            List<UserDTO> users = userRepository.getUsersByType(type);
+            System.out.println(users);
+            return users;
+        }
+        catch (DatabaseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    /**
+     * Show/print the users with the given location id
+     * @param id of the user to be shown.
+     */
+    //show users by user location
+    public List<UserDTO> UserFilteredWithLocation(int id){
+
+        JDBCUserRepository userRepository = new JDBCUserRepository();
+
+        try {
+            List<UserDTO> users = userRepository.getUsersByLocation(id);
+            System.out.println(users);
+            return users;
+        }
+        catch (DatabaseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Show/print the users with the given department id
+     * @param id of the user to be shown.
+     */
+    //show usesr by user department
+    public List<UserDTO> UserFilteredWithDepartment(int id){
+
+        JDBCUserRepository userRepository = new JDBCUserRepository();
+
+        try {
+            List<UserDTO> users = userRepository.getUsersByDepartment(id);
+            System.out.println(users);
+            return users;
+        }
+        catch (DatabaseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Show/print the users with the given start study year
+     * @param year of the user to be shown.
+     */
+    //show users by start study year
+    public List<UserDTO> UserFilteredWithStartStudyYear(int year){
+
+        JDBCUserRepository userRepository = new JDBCUserRepository();
+
+        try {
+            List<UserDTO> users = userRepository.getUsersByStartStudyYear(year);
+            System.out.println(users);
+            return users;
+        }
+        catch (DatabaseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Show/print the users with the given start work year
+     * @param year of the user to be shown.
+     */
+    //show users by start work year
+    public List<UserDTO> UserFilteredWithStartWorkYear(int year){
+
+        JDBCUserRepository userRepository = new JDBCUserRepository();
+
+        try {
+            List<UserDTO> users = userRepository.getUsersByStartWorkYear(year);
+            System.out.println(users);
+            return users;
+        }
+        catch (DatabaseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Show/print the users with the given usertype location and department
+     * @param type
+     * @param lId
+     * @param dId
+     * of the user to be shown.
+     */
+    //show users by loc dep and type
+    public List<UserDTO> UserFilterByTypeLocationAndDepartment(UserType type, int lId, int dId){
+
+        JDBCUserRepository userRepository = new JDBCUserRepository();
+
+        try {
+            List<UserDTO> users = userRepository.getUsersByUserTypeAndLocationAndDepartment(type, lId, dId);
+            System.out.println(users);
+            return users;
+        }
+        catch (DatabaseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Show/print the users with the given usertype start study year location and department
+     * @param type
+     * @param year
+     * @param lId
+     * @param dId
+     * of the user to be shown.
+     */
+    //show users by location user type location  department and start study year
+    public List<UserDTO> UserFilterByTypeLocationDepartmentAndStartSudyYear(UserType type, int year, int lId, int dId){
+
+        JDBCUserRepository userRepository = new JDBCUserRepository();
+
+        try {
+            List<UserDTO> users = userRepository.getUsersByUserTypeAndStartStudyYearAndDepartmentAndLocation(type, year, lId, dId);
+            System.out.println(users);
+            return users;
+        }
+        catch (DatabaseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Show/print the users with the given usertype start work year location and department
+     * @param type
+     * @param year
+     * @param lId
+     * @param dId
+     * of the user to be shown.
+     */
+    //show users by location user type  location department and start work year
+    public List<UserDTO> UserFilterByTypeLocationDepartmentAndStartWorkyearFontysStaff(UserType type, int year, int lId, int dId){
+
+        JDBCUserRepository userRepository = new JDBCUserRepository();
+
+        try {
+            List<UserDTO> users = userRepository.getUsersByUserTypeAndStartWorkYearAndDepartmentAndLocationFontysStaff(type, year, lId, dId);
+            System.out.println(users);
+            return users;
+        }
+        catch (DatabaseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /****************************RANIM*****************************Normal searching*********************/
+    /**
+     * Show/print the users of FontysIn Web Application
+     */
+    public List<UserDTO> GetAllUsers(){
+
+        JDBCUserRepository userRepository = new JDBCUserRepository();
+
+        try {
+            List<UserDTO> users = userRepository.getUsersDTO();
+            System.out.println(users);
+            return users;
+        }
+        catch (DatabaseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /***********************************Combine filter searching with the input box*************************************/
+    /**
+     * Show/print the users with the given usertype start work year location and department
+     * @param type
+     * @param lId
+     * @param dId
+     * @param chars
+     * of the user to be shown.
+     */
+    //show users by location user type department and start study year
+    public List<UserDTO> UserFilteLocationDepartmentTypeAndName(String chars, int lId, int dId, UserType type ){
+
+        JDBCUserRepository userRepository = new JDBCUserRepository();
+
+        try {
+            List<UserDTO> users = userRepository.getUsersByUserTypeLocationDeoartmentAndName(chars, lId, dId, type);
+            System.out.println(users);
+            return users;
+        }
+        catch (DatabaseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Show/print the users with the given usertype start work year location and department
+     * @param chars
+     * of the user to be shown.
+     */
+    //show users by location user type department and start study year
+    public List<UserDTO> UserFilterByFirstNameChars(String chars){
+
+        JDBCUserRepository userRepository = new JDBCUserRepository();
+
+        try {
+            List<UserDTO> users = userRepository.getUsersByFirstNameChars(chars);
+            System.out.println(users);
+            return users;
+        }
+        catch (DatabaseException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
