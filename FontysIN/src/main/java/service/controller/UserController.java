4307e01b244813d0befb9598b62c1fd9b7119168
Forslund, Beatrice I.B <b.forslund@student.fontys.nl>
2020-12-14 11:22:22 +0100

    fixed removed things

--- FontysIN/src/main/java/service/controller/UserController.java
+++ FontysIN/src/main/java/service/controller/UserController.java
@@ -57,7 +57,6 @@ public class UserController {
         }
         return false;
     }
-
     public User getUserFromToken(String token) {
         Claims decoded = decodeJWT(token);
 
@@ -66,6 +65,47 @@ public class UserController {
 
         return u;
     }
+    private static String SECRET_KEY = "oeRaYY";
+
+    //Sample method to construct a JWT
+    public String createJWT(String id, String issuer, String subject, long ttlMillis) {
+
+        //The JWT signature algorithm we will be using to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        //We will sign our JWT with our ApiKey secret
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        //Let's set the JWT Claims
+        JwtBuilder builder = Jwts.builder().setId(id)
+                .setIssuedAt(now)
+                .setSubject(subject)
+                .setIssuer(issuer)
+                .signWith(signatureAlgorithm, signingKey);
+
+//        //if it has been specified, let's add the expiration
+//        if (ttlMillis >= 0) {
+//            long expMillis = nowMillis + ttlMillis;
+//            Date exp = new Date(expMillis);
+//            builder.setExpiration(exp);
+//        }
+
+        //Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+    }
+    public  Claims decodeJWT(String jwt) {
+
+        //This line will throw an exception if it is not a signed JWS (as expected)
+        Claims claims = Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .parseClaimsJws(jwt).getBody();
+        return claims;
+    }
+
     public boolean addUser(User user) {
 
         try {
