ee41309711b17ec91eff2fedfb0f2f35be05b96e
Forslund, Beatrice I.B <b.forslund@student.fontys.nl>
2020-12-15 12:56:06 +0100

    test added

--- FontysIN/src/test/java/service/PrivacyTest.java
+++ FontysIN/src/test/java/service/PrivacyTest.java
@@ -1,4 +1,12 @@
 package service;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrivacyTest {
+    @Test
+   public void firstTest(){
+        assertEquals(1, 1);
+    }
 }
